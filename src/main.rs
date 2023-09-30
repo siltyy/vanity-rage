c7ea2cae70ca416a0e7adfd3da6b6711f20b4362
silt <silt@0xC3.win>
2023-09-30 06:28:21 -0400

    dont forget to license your software kids

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+/*
+  vanity-rage – a vanity key bruteforcer for age
+  Copyright (C) 2023 silt
+
+  This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+  Affero General Public License as published by the Free Software Foundation, either version 3 of
+  the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License along with this program.
+  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use age::{self, secrecy::ExposeSecret, Identity, Recipient};
 use glob_match::glob_match;
 use rayon::prelude::*;
